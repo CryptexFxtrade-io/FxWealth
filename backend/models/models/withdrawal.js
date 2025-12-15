bc79765d708b4e98f0145818bc16052c9cb9de8d
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-16 00:58:27 +0100

    Delete backend/models/models/withdrawal.js

--- backend/models/models/withdrawal.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Withdrawal", new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  amount: Number,
-  address: String,
-  status: { type: String, default: "pending" }
-}));

211c69ac0828f6266efbb96e39cd803b1e05dd8c
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-16 17:35:21 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,9 @@
-// backend/models/User.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, default: 'user' }
 });
-
-// Hash password before saving
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-// Compare password
-UserSchema.methods.comparePassword = async function(password) {
-  return await bcrypt.compare(password, this.password);
-};
 
 module.exports = mongoose.model('User', UserSchema);

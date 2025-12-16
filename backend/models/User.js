0f736433c4a78f23f5d79dbc3399f9edcde16b86
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-16 06:26:39 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  balance: { type: Number, default: 0 },
-  role: { type: String, default: 'user' },
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);

9643afd4e0611ab9480beb9e4f8c2a4cf5d7a1ea
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-16 18:18:33 +0100

    Update Investment.js

--- backend/models/Investment.js
+++ backend/models/Investment.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const InvestmentSchema = new mongoose.Schema({
   userId: mongoose.Schema.Types.ObjectId,
   amount: Number,
+  plan: String,
   profit: { type: Number, default: 0 },
   status: { type: String, default: 'active' }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Investment', InvestmentSchema);

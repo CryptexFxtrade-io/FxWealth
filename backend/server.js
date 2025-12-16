9fc9a5bbac42b0464ef615462f85a06a288037a6
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-16 06:25:50 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ app.use('/api/auth', authRoutes);
 
 // Connect MongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Start server
 const PORT = process.env.PORT || 5000;

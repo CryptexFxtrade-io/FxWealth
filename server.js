7026d86b8c20f2986f0bbe3834a86e03a01f9cfa
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-17 03:17:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,11 +25,12 @@ app.get('/', (req, res) => {
   res.json({ message: 'FxWealth API is running' });
 });
 
-console.log('Mongo URI:', process.env.MONGO_URI);
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

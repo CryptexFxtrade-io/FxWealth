8a29bd066b50dda22e53c12cf9926c418127943c
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-17 01:06:41 +0100

    Update investment.js

--- backend/routes/investment.js
+++ backend/routes/investment.js
@@ -1,21 +1,8 @@
 const express = require('express');
-const auth = require('../middleware/auth');
-const Investment = require('../models/Investment');
-
 const router = express.Router();
 
-router.post('/', auth(['user']), async (req, res) => {
-  const inv = await Investment.create({
-    userId: req.user.id,
-    amount: req.body.amount,
-    plan: req.body.plan
-  });
-  res.json(inv);
-});
-
-router.get('/', auth(), async (req, res) => {
-  const data = await Investment.find({ userId: req.user.id });
-  res.json(data);
+router.get('/test', (req, res) => {
+  res.json({ message: 'Investment route works!' });
 });
 
 module.exports = router;

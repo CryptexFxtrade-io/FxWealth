02803613e7b8ffde65e8ebad10ff796bad6e225e
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-17 14:26:16 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
 

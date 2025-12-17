267b92233aaf89f018a58b08a8a451a145481fb0
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-17 17:04:59 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import Admin from './pages/Admin';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<div>Welcome to FxWealth</div>} />
       </Routes>
     </Router>
   );

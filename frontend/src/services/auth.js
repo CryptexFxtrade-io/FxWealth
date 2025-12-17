0274fbd672ee3e1005bf52ff663d1ed1ed6a0934
CryptexFxtrade-io <ikennachidiprince@gmail.com>
2025-12-17 15:01:38 +0100

    Create auth.js

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,16 @@
+import { api } from "../api";
+
+export const login = async (email, password) => {
+  const res = await api.post("/auth/login", { email, password });
+  return res.data;
+};
+
+export const register = async (name, email, password) => {
+  const res = await api.post("/auth/register", { name, email, password });
+  return res.data;
+};
+
+export const getUser = async () => {
+  const res = await api.get("/auth/user");
+  return res.data;
+};
